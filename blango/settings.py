4bec7174db4b9a5980a06504f575a94d91e9100d
Neil Copland <neil.copland@gmail.com>
2022-07-30 17:57:20 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -53,12 +53,17 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
       'blango_auth',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google'
   ]
 
   MIDDLEWARE = [
@@ -190,7 +195,14 @@ class Dev(Configuration):
         "handlers": ["console"],
         "level": "DEBUG",
     },
+
+
   }
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 class Prod(Dev):
     DEBUG = False
